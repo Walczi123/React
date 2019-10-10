89a945048a4154755c4d517143aed8d3e3647f60
Patryk <patryk.wal@onet.pl>
2019-10-10 18:00:57 +0200

    Lab1Ex1

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MyApp = () => (
   <div>
-    <h1>Minimal React  walczakp</h1>
+    <h3>Minimal React  walczakp</h3>
     <p>Bundle size: 62 bytes, Load time of the bundle: 32 ms, Last commit SHA1: 2b5414f951499ebad32b0f68a526c69d2d4c1c</p>  
   </div>
 )
