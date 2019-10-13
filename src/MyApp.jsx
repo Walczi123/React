f9cb7061fb74a079eba8603a6b53ab0211076928
Walczi123 <38495867+Walczi123@users.noreply.github.com>
2019-10-13 14:12:08 +0200

    Lab1Ex3

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 const generateArray = (n) =>(Array.from({length: n}, (v, k) => k+1))
+const generateRandomArray = (n) =>(Array.from({length: n}, (v, k) => Math.round(Math.random()*24)+1))
 
 const MyApp = () => (
   <div>
     <h3>Minimal React  walczakp</h3>
     <p>Bundle size: 62 bytes, Load time of the bundle: 32 ms, Last commit SHA1: 2b5414f951499ebad32b0f68a526c69d2d4c1c</p>  
-    <ol>
-      <li>Ex1. Generate an array of integers from 1 to 7</li>
-      <ul>{generateArray(7).map(element => <li>{element}</li>)}</ul>
-    </ol>
+    <ul>
+      <li>Ex2. Generate an array of integers from 1 to 7</li>
+      <ul>{generateArray(5).map(element => <li>{element}</li>)}</ul>
+      <li>Ex3. Generate an array of random numbers from 1 to 25</li>
+      <ul>{generateRandomArray(5).map(element => <li>{element}</li>)}</ul>
+    </ul>
   </div>
 )
 export default MyApp
