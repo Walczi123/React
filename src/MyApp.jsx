de143d6cbac684229de80dc03dac144b853c9082
Walczi123 <38495867+Walczi123@users.noreply.github.com>
2019-10-13 15:11:56 +0200

    Lab1Ex5

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const generateArray = (n) =>(Array.from({length: n}, (v, k) => k+1))
 const generateRandomArray = (n) =>(Array.from({length: n}, (v, k) => Math.round(Math.random()*24)+1))
-const examleArray = [2, 56, 23, 88, 17, 4];
+const examleArray1 = [2, 56, 23, 88, 17, 4];
+const examleArray2 =[2, 5, 8, 10];
+const SquareArray = (a) =>(Array.from(a,x=>Math.sqrt(x)))
 
 
 const MyApp = () => (
@@ -15,7 +17,9 @@ const MyApp = () => (
       <li>Ex3. Generate an array of random numbers from 1 to 25</li>
       <ul>{generateRandomArray(5).map(element => <li>{element}</li>)}</ul>
       <li>Ex4. Generate an array of elements that are larger than 15</li>
-      <ul>{examleArray.filter(element => element>15).map(element => <li>{element}</li>)}</ul>
+      <ul>{examleArray1.filter(element => element>15).map(element => <li>{element}</li>)}</ul>
+      <li>Ex5. Array with the square root values of each number [2, 5, 8, 10]</li>
+      <ul>{SquareArray(examleArray2).map(element => <li>{element}</li>)}</ul>
     </ul>
   </div>
 )
