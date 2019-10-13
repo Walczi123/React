e3828afdc218e760030dac1671cee21df036e5fe
Walczi123 <38495867+Walczi123@users.noreply.github.com>
2019-10-13 14:57:08 +0200

    Lab1Ex4

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const generateArray = (n) =>(Array.from({length: n}, (v, k) => k+1))
 const generateRandomArray = (n) =>(Array.from({length: n}, (v, k) => Math.round(Math.random()*24)+1))
+const examleArray = [2, 56, 23, 88, 17, 4];
+
 
 const MyApp = () => (
   <div>
@@ -12,6 +14,8 @@ const MyApp = () => (
       <ul>{generateArray(5).map(element => <li>{element}</li>)}</ul>
       <li>Ex3. Generate an array of random numbers from 1 to 25</li>
       <ul>{generateRandomArray(5).map(element => <li>{element}</li>)}</ul>
+      <li>Ex4. Generate an array of elements that are larger than 15</li>
+      <ul>{examleArray.filter(element => element>15).map(element => <li>{element}</li>)}</ul>
     </ul>
   </div>
 )
