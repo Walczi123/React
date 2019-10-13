2f257554e5d6b15db22e93b0ed209b83a7f01528
Walczi123 <38495867+Walczi123@users.noreply.github.com>
2019-10-13 12:25:54 +0200

    Lab1Ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+const generateArray = (n) =>(Array.from({length: n}, (v, k) => k+1))
+
 const MyApp = () => (
   <div>
     <h3>Minimal React  walczakp</h3>
     <p>Bundle size: 62 bytes, Load time of the bundle: 32 ms, Last commit SHA1: 2b5414f951499ebad32b0f68a526c69d2d4c1c</p>  
+    <ol>
+      <li>Ex1. Generate an array of integers from 1 to 7</li>
+      <ul>{generateArray(7).map(element => <li>{element}</li>)}</ul>
+    </ol>
   </div>
 )
 export default MyApp
+
