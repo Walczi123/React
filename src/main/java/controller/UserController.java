caf986a83c13939e31aa19f2267237e163c923aa
Patryk <patryk.wal@onet.pl>
2019-10-27 18:55:13 +0100

    Task2 in progress

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import model.User;
+import service.UserService;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/spring-demo")
+public class UserController {
+	private UserService UserService;
+
+	@Autowired
+	public UserController(UserService UserService) {
+		this.UserService = UserService;
+	}
+
+	@GetMapping("/user")
+	public ResponseEntity<List<User>> getUser(@RequestParam(required = false) String login) {
+		List<User> result;
+		if (login != null && login.length() > 0)
+			result = UserService.findByLogin(login);
+		else
+			result = UserService.findAll();
+		if (result == null) {
+			throw new RuntimeException("user");
+		}
+
+		return ResponseEntity.ok(result);
+	}
+
+	@GetMapping("/user/{id}")
+	public ResponseEntity<User> getUser(@PathVariable(value = "id") int id) {
+		User result = UserService.findById(id);
+		if (result == null) {
+			throw new RuntimeException("id");
+		}
+
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/user/")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		if (UserService.exists(user)) {
+			throw new RuntimeException("login");
+		}
+
+		User result = UserService.save(user);
+
+		return ResponseEntity.ok(result);
+	}
+}
